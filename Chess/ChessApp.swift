9eccc90344b8683158bf59df16e628ebd5f3d0f6
Rodz Amor <ramorSTEM2022@gmail.com>
2022-08-19 15:08:30 -0400

    Initial Commit

---
+++ Chess/ChessApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChessApp.swift
+//  Chess
+//
+//  Created by Andrie on 8/19/22.
+//
+
+import SwiftUI
+
+@main
+struct ChessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
